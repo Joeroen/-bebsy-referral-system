f2a12b18129dd3df16ec2b062143748bbc62b2bb
Joeroen <joeroen@packxgen.com>
2025-06-22 18:59:06 +0200

    Create App.tsx

---
+++ Bebsy Project/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+typescriptimport React from 'react';
+import Dashboard from './bebsy_dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
